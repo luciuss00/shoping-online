e36f77a0db95fc84f92da0b3137f5e817f6190a4
Quang <luciuss710@gmail.com>
2026-03-20 23:19:05 +0700

    service_cart_service

---
+++ src/services/cartService.js
@@ -0,0 +1,21 @@
+import apiClient from '../api/productApi';
+
+const CartService = {
+    addToCart(email, nameProduct, quantity) {
+        const url = '/addProductToCart';
+        return apiClient.get(url, {
+            params: {
+                user: email,
+                nameProduct: nameProduct,
+                quantity: quantity,
+            },
+        });
+    },
+
+    getAllProductInCart(email) {
+        const url = '/userCheckList';
+        return apiClient.get(url, email);
+    },
+};
+
+export default CartService;
